2c44d546059ade8a3ba413dec07ea04acd811dda
Yujin Song <yshinny11@gmail.com>
2026-03-31 18:44:16 -0700

    learning the difference between model and schema

---
+++ app/post/post_model.py
@@ -0,0 +1,7 @@
+# DB table for post
+# - 게시글 번호   -> id         Int 숫자 (1씩 자동으로 증가하는 숫자)
+# - 게시글 제목    -> title         Str 문자 (숫자,영어, 특수문자 등)
+# - 게시글 내용    -> content      Str 문자 (숫자,영어, 특수문자 등)
+# - 작성자       -> writer       Int 숫자  
+# - 시간         -> created_at    Date 타입 (날짜타입)
+# - 수정시간    -> updated_at    Date 타입 (날짜타입)
